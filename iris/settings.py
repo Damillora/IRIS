8b280da9e652aec06a60ccac4f0ffdb6b46ddea9
Damillora <damillora@damillora.com>
2023-10-19 01:08:33 +0700

    Add basic throttling

--- iris/settings.py
+++ iris/settings.py
@@ -135,5 +135,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/second',
+    }
 }
